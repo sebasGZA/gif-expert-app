d437aa482dd7bddb009ce84ed6f7e579bdc15d7f
= <=>
2022-02-27 16:05:05 -0500

    GifExpertApp - Component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { GifExpertApp } from './GifExpertApp';
+import './index.css'
 
-ReactDOM.render(
-  <GifExpertApp />,
-  document.getElementById('root')
+ReactDOM.render( <
+    GifExpertApp / > ,
+    document.getElementById('root')
 );

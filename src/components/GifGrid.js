4de2ee86d337460233584cd9cc70f66056826216
= <=>
2022-03-01 15:05:45 -0500

    useEffect

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const GifGrid = (({ category }) => {
+
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        getGifs()
+    }, [])
 
     const getGifs = async () => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=rick+and+morty&limit=10&api_key=5GuCR301cgCkOZFLLowHX8RaIRMx0QdO'
@@ -19,13 +25,13 @@ export const GifGrid = (({ category }) => {
         console.log(gifs)
     }
 
-
-    getGifs()
     return (
         <div>
             <h3>
                 {category}
             </h3>
+            <h3>{count}</h3>
+            <button onClick={() => setCount(count + 1)}></button>
         </div>
     )
 })

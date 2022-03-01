fa78321ca1f3417769b15faf81096f42f6b89278
= <=>
2022-03-01 16:49:53 -0500

    Custom Hook

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,28 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import { getGifs } from '../helpers/getGifs';
-import { GifGridItem } from './GifGridItem';
+// import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+// import { getGifs } from '../helpers/getGifs';
+// import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = (({ category }) => {
 
-    const [images, setImages] = useState([])
+    // const [images, setImages] = useState([])
+    const { data, loading } = useFetchGifs();
+    console.log(data, loading)
 
-    useEffect(() => {
-        getGifs(category).then(imgs => setImages(imgs))
-    }, [category])
+    // useEffect(() => {
+    //     getGifs(category).then(imgs => setImages(imgs))
+    // }, [category])
 
     return (
         <>
             <h3>
                 {category}
             </h3>
-            <div className='cardGrid'>
+            {loading ? 'Cargando...' : 'Data cargada'}
+            {/* <div className='cardGrid'>
 
                 {
                     images.map(image => (
                         <GifGridItem key={image.id} {...image} />
                     ))
                 }
-            </div>
+            </div> */}
         </>
 
     )

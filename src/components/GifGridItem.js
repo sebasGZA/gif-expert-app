51abf53adfedfef162374e88f3a3d4194359c698
= <=>
2022-03-02 16:23:41 -0500

    Implementando PropTypes y Actualizar Snapshots

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 export const GifGridItem = ( {id ,title, url} ) => {
 
     return (
-        <div className='card animate__animated animate__backInRight'>
+        <div key={id} className='card animate__animated animate__backInRight'>
             <img src={url} alt={title}></img>
             <p>{title}</p>
             {/* {img.title} */}
         </div>
     )
+}
+
+GifGridItem.propTypes = {
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
 }
--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -5,9 +5,9 @@ import { GifGridItem } from '../../components/GifGridItem'
 describe('Pruebas para <GifGridItem/>', () => {
     test('Debe de mostrar <GifGridItem/> correctamente', () => {
         const id = '1234527'
-        const tittle = 'Dragon Ball'
+        const title = 'Dragon Ball'
         const url = 'https://media1.giphy.com/media/19JSJ5ucu91R5D7a3w/giphy.gif?cid=e81b06cevwgyupb28o4v4fs2te5t9r059ccn5dw2gxljzdwj&rid=giphy.gif&ct=g'
-        const wrapper = shallow(<GifGridItem id={id} title={tittle} url={url}/>)
+        const wrapper = shallow(<GifGridItem id={id} title={title} url={url}/>)
         expect(wrapper).toMatchSnapshot()
     })
 })

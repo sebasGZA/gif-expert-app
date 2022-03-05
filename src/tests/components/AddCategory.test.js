741a2720830423ca34001f27764c1b8bd29ffc8b
= <=>
2022-03-05 08:40:36 -0500

    Simular submit llamando la función y limpiando el inputValue

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -35,4 +35,23 @@ describe('Pruebas en <AddCategory/>', () => {
 
         expect(setCategories).not.toHaveBeenCalled()
     })
+
+    test('Debe de llamar el setCategories y limpiar la caja de texto', () => {
+        const value = 'Hola mundo'
+        const input = wrapper.find('input')
+        input.simulate('change', {
+            target: { value }
+        })
+        expect(wrapper.find('p').text().trim()).toBe(value)
+
+        wrapper.find('form').simulate('submit', {
+            preventDefault() { }
+        })
+
+        expect(setCategories).toHaveBeenCalled()
+        expect(setCategories).toHaveBeenCalledTimes(1)
+        expect(setCategories).toHaveBeenCalledWith(expect.any(Function))
+
+        expect(wrapper.find('input').prop('value')).toBe('')
+    })
 })

ac6845de0c82c8ac3179fbbe29ea1f2c6dfadf6b
sebasGZA <sebassga34@gmail.com>
2023-03-20 11:12:49 -0500

    Create a category list

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 export const GiftExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
   return (
     <>
       <h1>GiftExpertApp</h1>
+
+      <ol>
+        {categories.map((category, i) => {
+          return <li key={i}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };

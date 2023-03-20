50ff24312cd0cecdbb27c3f16aa80d3d13e75e2d
sebasGZA <sebassga34@gmail.com>
2023-03-20 11:18:59 -0500

    add new category

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -3,9 +3,16 @@ import { useState } from "react";
 export const GiftExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch"]);
 
+  const onAddCategory = () => {
+    //   setCategories((cat) => [...cat, "Dragon Ball"]);
+    setCategories(["Dragon Ball", ...categories]);
+  };
+
   return (
     <>
       <h1>GiftExpertApp</h1>
+
+      <button onClick={onAddCategory}>Add</button>
 
       <ol>
         {categories.map((category, i) => {

11efee6c21d0d79cc126fe2072f9768645c2b485
sebasGZA <sebassga34@gmail.com>
2023-03-20 13:23:42 -0500

    show loading message

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -8,6 +8,8 @@ export const GifGrid = ({ category }) => {
     <>
       <h3>{category}</h3>
 
+      {isLoading && <h2>Loading...</h2>}
+      
       <div className="card-grid">
         {images.map((image) => (
           <GifItem key={image.id} {...image} />

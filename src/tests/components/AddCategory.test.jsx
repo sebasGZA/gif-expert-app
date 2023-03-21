12d6c5892869680af19011641e4d39fd843483f2
sebasGZA <sebassga34@gmail.com>
2023-03-20 23:28:14 -0500

    tests: <AddCategory />

--- src/tests/components/AddCategory.test.jsx
+++ src/tests/components/AddCategory.test.jsx
@@ -1,10 +1,29 @@
-import { render } from "@testing-library/react";
+import { fireEvent, getByRole, render, screen } from "@testing-library/react";
 import { AddCategory } from "../../components/AddCategory";
 
 describe("Test in <AddCategory />", () => {
+  const inputValue = "Dragon Ball";
   test("Should make match with snapshot", () => {
-    const { container } = render(<AddCategory />);
+    const { container } = render(<AddCategory onNewCategory={() => {}} />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  test("Should change the textfield value", () => {
+    render(<AddCategory onNewCategory={() => {}} />);
+    const input = screen.getByRole("textbox");
+    fireEvent.input(input, { target: { value: inputValue } });
+
+    expect(input.value).toBe(inputValue);
+  });
+
+  test("Should submit the form", () => {
+    render(<AddCategory onNewCategory={() => {}} />);
+    const input = screen.getByRole("textbox");
+    const form = screen.getByRole("form");
+    fireEvent.input(input, { target: { value: inputValue } });
+    fireEvent.submit(form);
+
+    expect(input.value).toBe("");
   });
 });

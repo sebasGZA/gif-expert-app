5309e6756670754d1bb4c0935e91dad5d67b2006
sebasGZA <sebassga34@gmail.com>
2023-03-20 23:04:44 -0500

    tests: <GifItem />

--- src/tests/components/GifItem.test.jsx
+++ src/tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../components/GifItem";
 
 describe("Tests in <GifItem/>", () => {
@@ -8,5 +8,23 @@ describe("Tests in <GifItem/>", () => {
   test("Should make match with snapshot", () => {
     const { container } = render(<GifItem title={title} url={url} />);
     expect(container).toMatchSnapshot();
+  });
+
+  test("Should show the image with the url and the alt sent", () => {
+    render(<GifItem title={title} url={url} />);
+
+    // expect(screen.getByRole("img").src).toBe(url);
+    // expect(screen.getByRole("img").alt).toBe(title);
+
+    const { src, alt } = screen.getByRole("img");
+
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+  });
+
+  test("Should show title into the component", () => {
+    render(<GifItem title={title} url={url} />);
+
+    expect(screen.getByText(title)).toBeTruthy();
   });
 });
